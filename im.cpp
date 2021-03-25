2f063ac3737d9ea119817cc73ea28e53ade09475
YuSheng-qxx <62830655+YuSheng-qxx@users.noreply.github.com>
2021-03-25 15:45:29 +0800

    提交代码

---
+++ im.cpp
@@ -0,0 +1,23 @@
+#include "im.hpp"
+
+void test()
+{
+  im::TableUser user;
+  //user.Insert("lisi", "111111");
+  //user.UpdatePasswd("zhangsan", "1111111");
+  //cout<<user.VerifyUser("lisi", "111111")<<endl;
+  //cout<<user.Exises("zhangsan")<<endl;
+  Json::Value value;
+  user.SelectAll(&value);
+  Json::StyledWriter writer;
+  //cout<<writer.write(value)<<endl; 
+  user.Delete("lisi");
+}
+int main()
+{
+  im::IM im_server;
+  im_server.Init();
+  im_server.Run();
+  //test();
+  return 0;
+}
